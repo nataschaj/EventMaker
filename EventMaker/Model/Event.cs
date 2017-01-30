0e5e36666287c147420a66a3cb9e298384e63a36
Natascha Jakobsen <natascha_jakobsen@hotmail.com>
2017-01-30 13:06:30 +0100

    Add project files.

---
+++ EventMaker/Model/Event.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventMaker.Model
+{
+    public class Event
+    {
+        int id;
+        string Name;
+        string Description;
+        string Place;
+        DateTime DateTime;
+
+
+        public Event()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return id + " " + Name + " " + Description + " " + Place + " " + DateTime;
+        }
+    }
+}
