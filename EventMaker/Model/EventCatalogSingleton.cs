77187b5c5487b5c63c73dc149ddc53b30f11bee3
natsachaj <natascha_jakobsen@hotmail.com>
2017-01-30 13:35:09 +0100

    update

--- EventMaker/Model/EventCatalogSingleton.cs
+++ EventMaker/Model/EventCatalogSingleton.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace EventMaker.Model
 {
-    class EventCatalogSingleton
+    public class EventCatalogSingleton
     {
     }
 }
--- EventMaker/ViewModel/EventViewModel.cs
+++ EventMaker/ViewModel/EventViewModel.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EventMaker.Model;
+using System.ComponentModel;
 
 namespace EventMaker.ViewModel
 {
-    class EventViewModel
+    public class EventViewModel
     {
+        public Model.EventCatalogSingleton SingletonRef { get; set; }
     }
 }
